bea7aa6e03f780527a35784cdb9b4dce5453babd
devlucascalado <devlucascalado@gmail.com>
2025-06-20 17:31:29 -0300

    commit

--- crudlucas/src/main/java/com/example/crudlucas/model/Usuario.java
+++ crudlucas/src/main/java/com/example/crudlucas/model/Usuario.java
@@ -2,8 +2,6 @@ package com.example.crudlucas.model;
 
 import jakarta.persistence.*;
 
-
-
 @Entity(name = "usuario")
 @Table(name = "tb_usuario")
 public class Usuario {
@@ -52,36 +50,36 @@ public class Usuario {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getSobrenome() {
         return sobrenome;
     }
 
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getTelefone() {
         return telefone;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public void setTelefone(String telefone) {
